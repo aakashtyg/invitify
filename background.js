bf591f1e1d9967d86f8f35d2ffd7046101f0caed
aakash-cr7 <aakash.tyg93@gmail.com>
2016-06-15 03:13:03 +0530

    remove unwanted comments

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // Trigger some event on clicking the browser action button
 chrome.browserAction.onClicked.addListener(function(activeTab) {
-    // code below is supposed to be inside your button trigger
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         // Send message to content script to execute the script
         chrome.tabs.sendMessage(tabs[0].id, { status: "execute" }, function(response) {
