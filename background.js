2aad49b25385018a0238f34136de8d6593e81896
Aakash Tyagi <aakash.tyg93@gmail.com>
2017-03-17 23:21:05 +0530

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         // Send message to content script to execute the script
         chrome.tabs.sendMessage(tabs[0].id, { status: "execute" }, function(response) {
-            // Respnse from, the content script after receiving the message
+            // Response from, the content script after receiving the message
             if(response.status == "success"){
                 console.log('Script executed successfully');
             }
